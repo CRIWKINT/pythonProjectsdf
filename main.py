ac915cc17bed899aadc9f240bc7acd83053f9d78
CRIWKINT <chris.oe@hotmail.com>
2023-08-03 13:03:41 +0200

    Lille omting

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+from dateutil.parser import parse
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+df = pd.read_excel('IRData.xlsx', engine="openpyxl", parse_dates= ['Effective Date'])
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-# Jajajaja
+#Plot
+x = df['Effective Date'].values
+y1 = df['Rate (%)'].values
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+### Plot
+fig, ax = plt.subplots(1, 1, figsize=(10,5), dpi= 120)
+plt.plot(x, y1, color='tab:red')
+plt.ylim(-0.5, 8)
+plt.title('SOFR rates', fontsize=16)
+plt.hlines(y=0, xmin=np.min(df['Effective Date']), xmax=np.max(df['Effective Date']), linewidth=.5)
+plt.show()
